17fcd74c2d7fdd4e36ca579d92a4932a55a26cb7
Rahul T <rahulmanojcoder@gmail.com>
2023-03-11 19:17:17 +0530

    update

--- src/components/RegistrationForm/FemaleCategory.js
+++ src/components/RegistrationForm/FemaleCategory.js
@@ -1,4 +1,4 @@
-import { Checkbox, Form } from "antd";
+import { Checkbox, Col, Form, Row, Space } from "antd";
 
 const FemaleCategory = () => {
     const categories = [
@@ -14,11 +14,18 @@ const FemaleCategory = () => {
             value: "classic",
             label: "Women's Classic Physique Category",
         },
-
     ];
     return (
         <Form.Item name={["categories", "women"]}>
-            <Checkbox.Group options={categories} />
+            <Checkbox.Group>
+                <Space direction="vertical">
+                    {categories.map((category) => (
+                        <Checkbox style={{padding: "10px 0"}} key={category.value} value={category.value}>
+                            {category.label}
+                        </Checkbox>
+                    ))}
+                </Space>
+            </Checkbox.Group>
         </Form.Item>
     );
 };
