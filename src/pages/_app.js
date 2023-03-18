e6962cfbd5de879b56f1cb3b426b1b43a4380b8f
Rahul T <rahulmanojcoder@gmail.com>
2023-03-18 13:14:06 +0530

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,22 @@
 import Layout from "@/components/Layout";
-import "./global.scss"
-import "@/components/RegistrationForm/RegistrationForm.scss"
-import NextNProgress from 'nextjs-progressbar';
+import "./global.scss";
+import "@/components/RegistrationForm/RegistrationForm.scss";
+import NextNProgress from "nextjs-progressbar";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
     return (
         <Layout>
+            <DefaultSeo
+                title="Hardwork Classic"
+                description="Hard work classic"
+                openGraph={{
+                    type: "website",
+                    locale: "en_IE",
+                    url: "https://hardworkclassic.com/",
+                    siteName: "HardWork Classic",
+                }}
+            />
             <NextNProgress />
             <Component {...pageProps} />
         </Layout>
